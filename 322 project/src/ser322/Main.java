e81dbe2944342e112b93a13783f997c12063f301
Ryan Munin <rmgarc31@asu.edu>
2021-02-23 14:22:10 -0800

    Scanner added with usage reminders.

--- 322 project/src/ser322/Main.java
+++ 322 project/src/ser322/Main.java
@@ -1,7 +1,19 @@
 package ser322;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        // This is a scanner we can use for user input.
+        Scanner inputScanner = new Scanner(System.in);
+
+        // Let the user know what we want
+        System.out.println("We ask for input like this");
+
+        // Read an entire line of user input. Can be adapted with next() or
+        // integer.parseint for more specific requirements.
+        String inString = inputScanner.nextLine();
 
     }
 
