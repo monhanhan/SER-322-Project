8417a27ec1fbfab2281b59c2cf97aa6ccbd4dfbc
Ryan Munin <rmgarc31@asu.edu>
2021-02-24 22:47:49 -0800

    Main menu option added

--- 322 project/src/ser322/Main.java
+++ 322 project/src/ser322/Main.java
@@ -27,6 +27,10 @@ public class Main {
 
             } else if (inInt == 2) {
                 Albums.albumsMenu(inputScanner, args);
+
+            } else if (inInt == 3) {
+                Bands.bandsMenu(inputScanner, args);
+
             } else {
                 System.out.println(
                         "I'm sorry, you have not selected a valid option.");
@@ -54,6 +58,7 @@ public class Main {
         System.out.println("0: Exit System");
         System.out.println("1: Artists");
         System.out.println("2: Albums");
+        System.out.println("3: Bands");
         System.out.println();
 
     }
